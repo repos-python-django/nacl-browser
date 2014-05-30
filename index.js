1ab8bfb50d8f9a413c013dd63d5e4dc66c25c2c4
Tim Taubert <tim@timtaubert.de>
2014-05-30 12:16:23 +0200

    move code to src/ dir

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-mix(require("./hash.js"), module.exports);
-mix(require("./auth.js"), module.exports);
-mix(require("./stream.js"), module.exports);
+mix(require("./src/hash.js"), module.exports);
+mix(require("./src/auth.js"), module.exports);
+mix(require("./src/stream.js"), module.exports);
 
 function mix(from, into) {
   for (var key in from) {
